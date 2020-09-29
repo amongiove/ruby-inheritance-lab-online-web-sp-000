5ee686dca934ba0e1886d89ff47f686dfa2d7898
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-29 03:04:19 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,14 @@
 class User
-  attr_accessor :first_name, :last_name
+  attr_accessor :name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(name)
+    @name = name
   end
 
+  def first_name(name)
+    first_name = @name.split[0]
+  end
+
+  def last_name(name)
+    last_name = @name.split[1]
 end
